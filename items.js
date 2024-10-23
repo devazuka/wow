edb3994d115a22f862af756f0c42a54268fbaca3
Clement Denis <clement@01talent.com>
2024-10-23 22:30:41 +0100

    add items & spells

---
+++ items.js
@@ -0,0 +1,82 @@
+export const starterVendor = [
+  // Weapons:
+  20434, // Lorekeeper's Staff (Alliance)
+  20425, // Advisor's Gnarled Staff (Horde)
+  890, // Twisted Chanter's Staff
+  2271, // Staff of the Blessed Seer
+  7230, // Smite's Mighty Hammer
+  1318, // Night Reaver
+  935, // Night Watch Shortsword
+  6633, // Butcher's Slicer
+  1937, // Buzz Saw
+  5196, // Smite's Reaver
+  6472, // Stinging Viper
+  5197, // Cookie's Tenderizer
+  1483, // Face Smasher
+  6448, // Tail Spike
+  2169, // Buzzer Blade
+  29584, // Throat Piercers
+  29201, // Thick Bronze Darts
+  31270, // Banshee Rod of the Sun
+  2879, // Antipodean Rod
+  5183, // Pulsating Hydra Heart
+  6341, // Eerie Stable Lantern
+  6383, // Forest Buckler
+  // 15512, // Grunt's Shield (only all low random enchant) ex: only +3 strength not the +4
+
+  // Cloth:
+  4385, // Green Tinted Goggles (no need engi)
+  4373, // Shadow Goggles (no need engi)
+  4315, // Reinforced Woolen Shoulders
+  1486, // Tree Bark Jacket
+  5202, // Corsair's Overshirt
+  6465, // Robe of the Moccasin
+  6226, // Bloody Apron
+  14150, // Robe of Evocation
+  // 9768, // Greenweave Bracers (only all low random enchant) ex: only +4 spell power not the +5
+  5195, // Gold-flecked Gloves
+  5970, // Serpent Gloves
+  // 14162, // Pagan Mitts (only all low random enchant)
+  2911, // Keller's Girdle
+  // 9766, // Greenweave Sash (only all low random enchant)
+  // 6568, // Shimmering Trousers (only all low random enchant)
+  // 9767, // Greenweave Sandals (only all low random enchant)
+
+  // Leather:
+  5404, // Serpent's Shoulders
+  10399, // Blackened Defias Armor
+  6473, // Armor of the Fang
+  2314, // Toughened Leather Armor
+  12988, // Starsight Tunic
+  // 15331, // Wrangler's Wristbands (only all low random enchant)
+  // 6586, // Scouting Gloves (only all low random enchant)
+  10403, // Blackened Defias Belt
+  // 15329, // Wrangler's Belt (only all low random enchant)
+  // 6587, // Scouting Trousers (only all low random enchant)
+  // 6582, // Scouting Boots (only all low random enchant)
+
+  // Mail:
+  3480, // Rough Bronze Shoulders
+  // 15500, // Outrunner's Chestguard (only all low random enchant)
+  // 9811, // Fortified Bracers (only all low random enchant)
+  // 6577, // Defender Gauntlets (only all low random enchant)
+  6460, // Cobrahn's Grasp
+  // 9814, // Fortified Belt (only all low random enchant)
+  // 15511, // Grunt's Legguards (only all low random enchant)
+  // 6573, // Defender Boots (only all low random enchant)
+  6459, // Savage Trodders
+
+  // Jewelry:
+  21933, // Thick Bronze Necklace
+  20428, // Caretaker's Cape (Alliance)
+  20427, // Battle Healer's Cloak (Horde)
+  2059, // Sentry Cloak
+  6449, // Glowing Lizardscale Cloak
+  20820, // Simple Pearl Ring
+  // 11967, // Zircon Band (only all low random enchant)
+  4998, // Blood Ring
+  12996, // Band of Purification
+  40476, // Insignia of the Alliance (Alliance)
+  40477, // Insignia of the Horde (Horde)
+  19024, // Arena Grand Master (Custom) 0 stamina only use
+]
---
+++ spells.js
@@ -0,0 +1,199 @@
+export const startingSpell = {
+  warrior: [
+    71, // Defensive Stance
+    72, // Shield Bash
+    100, // Charge
+    285, // Heroic Strike
+    355, // Taunt
+    676, // Disarm
+    694, // Mocking Blow
+    1160, // Demoralizing Shout
+    1715, // Hamstring
+    2457, // Battle Stance
+    2565, // Shield Block
+    3127, // Parry
+    5242, // Battle Shout
+    6546, // Rend
+    6572, // Revenge
+    7384, // Overpower
+    7386, // Sunder Armor
+    8198, // Thunder Clap
+    29131, // Bloodrage
+    34428, // Victory Rush
+  ],
+
+  paladin: [
+    498, // Divine Protection
+    633, // Lay on Hands
+    647, // Holy Light
+    853, // Hammer of Justice
+    1022, // Hand of Protection
+    1044, // Hand of Freedom
+    1152, // Purify
+    3127, // Parry
+    7328, // Redemption
+    10290, // Devotion Aura
+    19742, // Blessing of Wisdom
+    19834, // Blessing of Might
+    20271, // Judgement of Light
+    21084, // Seal of Righteousness
+    25780, // Righteous Fury
+    31789, // Righteous Defense
+    53408, // Judgement of Wisdom
+    62124, // Hand of Reckoning
+  ],
+
+  hunter: [
+    75, // Auto Shot
+    136, // Mend Pet
+    883, // Call Pet
+    982, // Revive Pet
+    1002, // Eyes of the Beast
+    1130, // Hunter's Mark
+    1494, // Track Beasts
+    1495, // Mongoose Bite
+    1513, // Scare Beast
+    1515, // Tame Beast
+    2641, // Dismiss Pet
+    2643, // Multi-Shot
+    2974, // Wing Clip
+    3127, // Parry
+    5116, // Concussive Shot
+    5118, // Aspect of the Cheetah
+    6197, // Eagle Eye
+    6991, // Feed Pet
+    13163, // Aspect of the Monkey
+    13550, // Serpent Sting
+    13795, // Immolation Trap
+    14261, // Raptor Strike
+    14281, // Arcane Shot
+    14318, // Aspect of the Hawk
+    19883, // Track Humanoids
+    19884, // Track Undead
+    20736, // Distracting Shot
+  ],
+
+  rogue: [
+    703, // Garrote
+    921, // Pick Pocket
+    1758, // Sinister Strike
+    1766, // Kick
+    1776, // Gouge
+    1784, // Stealth
+    1804, // Pick Lock
+    1966, // Feint
+    2589, // Backstab
+    2983, // Sprint
+    3127, // Parry
+    5171, // Slice and Dice
+    5277, // Evasion
+    6761, // Eviscerate
+    6770, // Sap
+    8647, // Expose Armor
+    8676, // Ambush
+  ],
+
+  priest: [
+    527, // Dispel Magic
+    528, // Cure Disease
+    586, // Fade
+    588, // Inner Fire
+    598, // Smite
+    600, // Power Word: Shield
+    970, // Shadow Word: Pain
+    1244, // Power Word: Fortitude
+    2006, // Resurrection
+    2053, // Lesser Heal
+    2054, // Heal
+    6074, // Renew
+    8102, // Mind Blast
+    8122, // Psychic Scream
+  ],
+
+  shaman: [
+    325, // Lightning Shield
+    370, // Purge
+    526, // Cure Toxins
+    548, // Lightning Bolt
+    913, // Healing Wave
+    1535, // Fire Nova
+    2008, // Ancestral Spirit
+    2484, // Earthbind Totem
+    2645, // Ghost Wolf
+    3599, // Searing Totem
+    6390, // Stoneclaw Totem
+    8019, // Rockbiter Weapon
+    8027, // Flametongue Weapon
+    8045, // Earth Shock
+    8052, // Flame Shock
+    8075, // Strength of Earth Totem
+    8143, // Tremor Totem
+    8154, // Stoneskin Totem
+    57994, // Wind Shear
+  ],
+
+  mage: [
+    118, // Polymorph
+    122, // Frost Nova
+    130, // Slow Fall
+    475, // Remove Curse
+    597, // Conjure Food
+    604, // Dampen Magic
+    837, // Frost Bolt
+    1008, // Amplify Magic
+    1449, // Arcane Explosion
+    1460, // Arcane Intellect
+    2120, // Flamestrike
+    2137, // Fire Blast
+    3140, // Fireball
+    5144, // Arcane Missiles
+    5505, // Conjure Water
+    7300, // Frost Armor
+  ],
+
+  warlock: [
+    688, // Summon Imp
+    689, // Drain Life
+    693, // Create Soulstone
+    696, // Demon Skin
+    697, // Summon Voidwalker
+    705, // Shadow Bolt
+    707, // Immolate
+    755, // Health Funnel
+    1014, // Curse of Agony
+    1108, // Curse of Weakness
+    1120, // Drain Soul
+    1455, // Life Tap
+    5676, // Searing Pain
+    5697, // Unending Breath
+    5782, // Fear
+    6201, // Create Healthstone
+    6222, // Corruption
+  ],
+
+  druid: [
+    99, // Demoralizing Roar
+    770, // Faerie Fire
+    779, // Swipe (Bear)
+    782, // Thorns
+    783, // Travel Form
+    1062, // Entangling Roots
+    1066, // Aquatic Form
+    1430, // Rejuvenation
+    2637, // Hibernate
+    5178, // Wrath
+    5187, // Healing Touch
+    5211, // Bash
+    5229, // Enrage
+    5232, // Mark of the Wild
+    5487, // Bear Form
+    6795, // Growl
+    6808, // Maul
+    8925, // Moonfire
+    8938, // Regrowth
+    8946, // Cure Poison
+    16810, // Nature's Grasp
+    16857, // Faerie Fire (Feral)
+    50769, // Revive
+  ],
+}
