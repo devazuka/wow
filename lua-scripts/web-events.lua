387070401c5964074977db03fa7aa0c5ae0ebbfb
Clement Denis <clement@01talent.com>
2024-10-27 01:08:09 +0100

    try to fix the ugly color

--- lua-scripts/web-events.lua
+++ lua-scripts/web-events.lua
@@ -200,7 +200,7 @@ RegisterPlayerEvent(PLAYER_EVENT_ON_CHANNEL_CHAT, function(event, player, msg, T
   if channel == 1 then
     -- Send message to players of both factions in the channel
     for _, targetPlayer in pairs(GetPlayersInWorld()) do
-      targetPlayer:SendBroadcastMessage("|cFF668800[general] |r" .. player:GetName() .. ": " .. msg)
+      targetPlayer:SendBroadcastMessage("[|cFFFFA500general|r]|cFFFFF5DE " .. player:GetName() .. ": " .. msg)
     end
     SendWebEvent('GENERAL_CHANNEL_MESSAGE', player, { message = msg })
     return false -- Prevents the message from duplicating in the original chat
